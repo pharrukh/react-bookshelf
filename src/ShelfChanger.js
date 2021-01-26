5c802ecbd80f49be7c17c47128007305fd2f4667
pharrukh <farrukh.normuradov@gmail.com>
2021-01-26 20:17:19 +0100

    chore: add static components

---
+++ src/ShelfChanger.js
@@ -0,0 +1,13 @@
+const ShelfChanger = () => (
+    <div className="book-shelf-changer">
+        <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+        </select>
+    </div>
+)
+
+export default ShelfChanger;
