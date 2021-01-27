304646caf0ce78bf2b6614dc187305cb5ffe8b59
pharrukh <farrukh.normuradov@gmail.com>
2021-01-27 18:36:18 +0100

    feat: final static version

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,10 +1,10 @@
-const ShelfChanger = () => (
+const ShelfChanger = ({ shelf }) => (
     <div className="book-shelf-changer">
-        <select>
+        <select value={shelf}>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
+            <option value="wantToRead" >Want to Read</option>
+            <option value="read" >Read</option>
             <option value="none">None</option>
         </select>
     </div>
